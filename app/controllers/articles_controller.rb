fedd3d0661703dd4cbd65b289ddefd0a6ce9eda6
vivekshukla <vivekshukla.nit@gmail.com>
2020-09-11 11:02:05 +0530

    Validation of fields and flash messages added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,14 +6,18 @@ def index
   @articles = Article.all
 end
 def new
-
+  @article = Article.new
 end
 def create
     
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    redirect_to article_path(@article)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
     #above line can also be written as redirect_to @article (shorthand syntax)
+    else
+    render 'new'
+    end
 end
 
 end

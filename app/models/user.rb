6fd6e43110640adc04f696f30c74e92125585ace
vivekshukla <vivekshukla.nit@gmail.com>
2020-09-12 18:59:44 +0530

    Changes for using BCrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ validates :username, presence: true,
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 validates :email, presence: true,
            format: {with: VALID_EMAIL_REGEX} 
+  has_secure_password
 end

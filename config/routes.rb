08ba8be5fc745d4c331ed7f7255f940d451174b6
vivekshukla <vivekshukla.nit@gmail.com>
2020-09-13 12:03:56 +0530

    new action added to signup users

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
 root 'pages#home'
 get 'about', to:'pages#about'
 resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+get 'signup', to: 'users#new'
+resources :users, except: [:new] #get all the routes for users except #new since we have accepted new action in above line
 end

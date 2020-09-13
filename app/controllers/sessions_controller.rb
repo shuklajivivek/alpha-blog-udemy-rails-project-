083625eac417c0ec4d2205c8a86a1462cba42303
vivekshukla <vivekshukla.nit@gmail.com>
2020-09-13 17:54:29 +0530

    login and logout sessions added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,21 @@ def new
 end
 
 def create
-    byebug
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+        session[:user_id] = user.id
+        flash[:notice] = "Logged In successfully"
+        redirect_to user
+    else
+        flash.now[:alert] = "Something wrong with your credentials"
+        render 'new'
+    end
 end
 
 def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Logged out successfully"
+    redirect_to root_path
 end
 
 end

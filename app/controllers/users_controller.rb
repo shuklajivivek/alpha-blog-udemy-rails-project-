a124d1f2e4b3393ed7a754690a5c2e3e918d37b6
vivekshukla <vivekshukla.nit@gmail.com>
2020-09-13 15:50:01 +0530

    Clean layouts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ def update
     @user = User.find(params[:id])
     if @user.update(user_params)
     flash[:notice] = "Profile successfully updated"
-    redirect_to articles_path
+    redirect_to @user #@user is short hand for user_path(userid)
   else
     render 'edit'
   end

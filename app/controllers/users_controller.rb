d9fb02a7ce9f88fe00aa269f984a0b93670b39e0
vivekshukla <vivekshukla.nit@gmail.com>
2020-09-13 14:17:25 +0530

    added Gravatar Profile Image to user's profile  and associated articles to that user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,10 @@ class UsersController < ApplicationController
 def new
     @user = User.new
 end
-
+def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+end
 def edit
     @user = User.find(params[:id])
 end

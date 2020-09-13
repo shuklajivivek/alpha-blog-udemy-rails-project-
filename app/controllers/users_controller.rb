4299c494c69b5df135a39abdd9e3b57b555a24cd
vivekshukla <vivekshukla.nit@gmail.com>
2020-09-13 18:29:26 +0530

    added Authentication helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,23 @@
 class UsersController < ApplicationController
+
+before_action :set_user, only: [:show, :edit, :update]
+
 def new
     @user = User.new
 end
+
 def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 2)
 end
+
 def edit
-    @user = User.find(params[:id])
 end
+
 def index
     @users = User.paginate(page: params[:page], per_page: 2)
 end
+
 def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
     flash[:notice] = "Profile successfully updated"
     redirect_to @user #@user is short hand for user_path(userid)
@@ -25,6 +29,7 @@ end
 def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Hi #{@user.username} You have sucessfully signed up for alpha-blog"
       redirect_to articles_path
     else
@@ -35,5 +40,8 @@ end
  private
  def user_params
     params.require(:user).permit(:username, :email, :password)
+ end
+ def set_user
+    @user = User.find(params[:id])
  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,13 @@ module ApplicationHelper
       gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
       image_tag(gravatar_url,alt:user.username, class: "rounded shadow mx-auto d-block")
     end
+
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+      # ||= is used for memoization of @current_user variable
+    end
+
+    def logged_in?
+      !!current_user #here !! converts @current_user into boolean
+    end
 end

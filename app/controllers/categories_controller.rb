94524e1c480a631bd9881a87703c303f5b21d8e6
vivekshukla <vivekshukla.nit@gmail.com>
2020-09-14 14:45:23 +0530

    Create Category and Test | Category Controller Test added

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,7 +5,23 @@ end
 def show
 end
 
-def new
+def new 
+    @category = Category.new   
+end
+
+def create
+  @category = Category.new(category_params)
+  if @category.save
+    flash[:notice] = "Category Created Successfully"
+    redirect_to @category
+  else
+    render 'new'
+  end
+end
+
+ private
+def category_params
+    params.require(:category).permit(:name)
 end
 
 end
